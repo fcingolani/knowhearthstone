ba8d080b605d63c75cd508f7533a4169194e3109
Federico Cingolani <mail@fcingolani.com.ar>
2016-12-26 10:02:23 -0300

    Update common.rb

--- common.rb
+++ common.rb
@@ -2,29 +2,11 @@ require 'sqlite3'
 require 'sequel'
 require 'logger'
 
-
-require 'twitter'
-# <3 https://mikecoutermarsh.com/the-io-object-for-media-must-respond-to-to_io/
-module Twitter::Image
-  # The Twitter gem is particular about the type of IO object it
-  #   recieves when tweeting an image. If an image is < 10kb, Ruby opens it as a
-  #   StringIO object. Which is not supported by the Twitter gem/api.
-  #
-  #   This method ensures we always have a valid IO object for Twitter.
-  def self.open_from_url(image_url)
-    image_file = open(image_url)
-    return image_file unless image_file.is_a?(StringIO)
-
-    file_name = File.basename(image_url)
-
-    temp_file = Tempfile.new(file_name)
-    temp_file.binmode
-    temp_file.write(image_file.read)
-    temp_file.close
-
-    open(temp_file.path)
-  end
-end
+# http://stackoverflow.com/a/6632032
+# Don't allow downloaded files to be created as StringIO. Force a tempfile to be created.
+require 'open-uri'
+OpenURI::Buffer.send :remove_const, 'StringMax' if OpenURI::Buffer.const_defined?('StringMax')
+OpenURI::Buffer.const_set 'StringMax', 0
 
 LOGGER = Logger.new($stdout)
 
